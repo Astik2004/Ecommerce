a68865785a49bb59c5bd304272618814fe17b0f3
Astik <astikyadav4@gmail.com>
2024-08-26 14:29:48 +0530

    resct

--- frontend/src/components/SignUp.js
+++ frontend/src/components/SignUp.js
@@ -1,11 +1,37 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import {useNavigate} from 'react-router-dom';
 
 function SignUp() {
     const [name,setName]=useState("");
     const [email,setEmail]=useState("");
-    const [pass,setPass]=useState("");
-    const collectData=()=>{
-        console.log(name,email,pass);
+    const [password,setPassword]=useState("");
+    const [error, setError] = useState(false);
+    const navigate=useNavigate();
+    useEffect(()=>{
+      const auth=localStorage.getItem('user');
+      if(auth){
+        navigate("/")
+      }
+    })
+    const collectData=async()=>{
+      if(!name || !email || !password){
+        setError(true);
+        return false;
+      }
+        console.log(name,email,password);
+        let result=await fetch("http://localhost:5000/register",{
+          method:'post',
+          body:JSON.stringify({name,email,password}),
+          headers:{
+              'Content-Type':'application/json'
+          }
+        });
+        result=await result.json();
+        console.warn(result);
+        localStorage.setItem("user",JSON.stringify(result.result));
+        localStorage.setItem("token",JSON.stringify(result.auth));
+        navigate("/")
+
     }
   return (
     <div className='register'>
@@ -13,12 +39,15 @@ function SignUp() {
       <input className='inputBox' type="text" placeholder="Enter Name"
         value={name} onChange={(e)=>setName(e.target.value)}
       />
+      {error && !name && <span className='invalid-input'>*Enter valid name</span>}
       <input className='inputBox' type="text" placeholder="Enter email"
         value={email} onChange={(e)=>setEmail(e.target.value)}
       />
+      {error && !email && <span className='invalid-input'>*Enter valid name</span>}
       <input className='inputBox' type="Password" placeholder="Enter Password"
-        value={pass} onChange={(e)=>setPass(e.target.value)}
+        value={password} onChange={(e)=>setPassword(e.target.value)}
       />
+      {error && !password && <span className='invalid-input'>*Enter valid name</span>}
       <button onClick={collectData} className='appButton' type='button'>SignUp</button>
     </div>
   )
